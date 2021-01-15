35a0a39fbc8c81cf3a6c24f73fce8fc986f480b1
Florian Mauracher <flo@mauracher.eu>
2021-01-15 01:37:39 +0100

    Cache tiles in service worker

--- src/sw.js
+++ src/sw.js
@@ -2,7 +2,25 @@ import { clientsClaim } from "workbox-core";
 import { registerRoute } from "workbox-routing";
 import { CacheFirst } from "workbox-strategies";
 import { ExpirationPlugin } from "workbox-expiration";
+import { CacheableResponsePlugin } from 'workbox-cacheable-response';
 import { cleanupOutdatedCaches, precacheAndRoute } from "workbox-precaching";
+
+registerRoute(
+  new RegExp("https://(?:api.maptiler.com|basemaps.cartocdn.com)/.*.png"),
+  new CacheFirst({
+    cacheName: "tile-cache",
+    plugins: [
+      new CacheableResponsePlugin({
+        statuses: [0, 200]
+      }),
+      new ExpirationPlugin({
+        maxEntries: 20000,
+        maxAgeSeconds: 7 * 24 * 60 * 60,
+        purgeOnQuotaError: true
+      })
+    ]
+  })
+);
 
 /* eslint-disable no-restricted-globals, no-underscore-dangle */
 cleanupOutdatedCaches();
