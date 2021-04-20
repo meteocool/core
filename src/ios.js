5ec778157803566588e76b4bb8e6f3242ee77c5d
Valentin Dornauer <valentin@unimplemented.org>
2021-04-20 03:55:33 +0200

    De-duplicate sentry code

--- src/ios.js
+++ src/ios.js
@@ -1,15 +1,6 @@
 import * as Sentry from "@sentry/browser";
-import { Integrations } from "@sentry/tracing";
-import { CaptureConsole } from "@sentry/integrations";
-
-Sentry.init({
-  dsn: "https://ee86f8a6a22f4b7fb267b01e22c07d1e@o347743.ingest.sentry.io/5481137",
-  integrations: [new Integrations.BrowserTracing(), new CaptureConsole({ levels: ["error"] })],
-  tracesSampleRate: 1.0,
-  environment: process.env.NODE_ENV,
-  autoSessionTracking: false,
-  release: GIT_COMMIT_HASH,
-});
+import SENTRY_ARGS from "./lib/sentry";
+Sentry.init(SENTRY_ARGS);
 
 import { Workbox } from "workbox-window";
 import App from "./App.svelte";
