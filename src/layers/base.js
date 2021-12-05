13b7f82262cfeed3f24fa73edcd821ae2cbef32d
Valentin Dornauer <valentin@unimplemented.org>
2021-12-05 18:29:06 +0100

    Improve stability by avoiding vector labels in LayerGroup

--- src/layers/base.js
+++ src/layers/base.js
@@ -2,41 +2,35 @@ import TileLayer from "ol/layer/Tile";
 import XYZ from "ol/source/XYZ";
 import { DEVICE_PIXEL_RATIO } from "ol/has";
 import OSM from "ol/source/OSM";
-import LayerGroup from "ol/layer/Group";
 import {
   cartoAttribution,
   osmAttribution,
   cyclosmAttribution,
 } from "./attributions";
-import { labelsOnly } from "./vector";
 
 const retina = DEVICE_PIXEL_RATIO > 1 ? "@2x" : "";
 
-export const cartoDark = () => new LayerGroup({
-  layers: [new TileLayer({
-    source: new XYZ({
-      url: `https://cartodb-basemaps-{a-c}.global.ssl.fastly.net/rastertiles/dark_nolabels/{z}/{x}/{y}${retina}.png`,
-      attributions: [osmAttribution, cartoAttribution],
-      maxZoom: 20,
-    }),
-    base: true,
-    preload: Infinity,
-    zIndex: 1,
-  }), labelsOnly()],
+export const cartoDark = () => new TileLayer({
+  source: new XYZ({
+    url: `https://cartodb-basemaps-{a-c}.global.ssl.fastly.net/rastertiles/dark_nolabels/{z}/{x}/{y}${retina}.png`,
+    attributions: [osmAttribution, cartoAttribution],
+    maxZoom: 20,
+  }),
+  base: true,
+  preload: Infinity,
+  zIndex: 1,
 });
 
-export const cartoLight = () => new LayerGroup({
-  layers: [new TileLayer({
-    source: new XYZ({
-      url: `https://cartodb-basemaps-{a-c}.global.ssl.fastly.net/rastertiles/voyager_nolabels/{z}/{x}/{y}${retina}.png`,
-      attributions: [osmAttribution, cartoAttribution],
-      tilePixelRatio: DEVICE_PIXEL_RATIO > 1 ? 2 : 1, // Retina support
-      maxZoom: 20,
-    }),
-    base: true,
-    preload: Infinity,
-    zIndex: 1,
-  }), labelsOnly()],
+export const cartoLight = () => new TileLayer({
+  source: new XYZ({
+    url: `https://cartodb-basemaps-{a-c}.global.ssl.fastly.net/rastertiles/voyager_nolabels/{z}/{x}/{y}${retina}.png`,
+    attributions: [osmAttribution, cartoAttribution],
+    tilePixelRatio: DEVICE_PIXEL_RATIO > 1 ? 2 : 1, // Retina support
+    maxZoom: 20,
+  }),
+  base: true,
+  preload: Infinity,
+  zIndex: 1,
 });
 
 export const osm = () => new TileLayer({
