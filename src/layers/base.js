c95d6bcfa0193860b1198a731e2ae19f1cf873c5
Valentin Dornauer <valentin@unimplemented.org>
2022-02-05 21:54:04 +0100

    Fix Firefox CORS issues (?)

--- src/layers/base.js
+++ src/layers/base.js
@@ -13,17 +13,20 @@ const retina = DEVICE_PIXEL_RATIO > 1 ? "@2x" : "";
 export const cartoDark = () => new TileLayer({
   source: new XYZ({
     url: `https://cartodb-basemaps-{a-c}.global.ssl.fastly.net/rastertiles/dark_nolabels/{z}/{x}/{y}${retina}.png`,
+    crossOrigin: "",
     attributions: [osmAttribution, cartoAttribution],
     maxZoom: 20,
   }),
   base: true,
   preload: Infinity,
   zIndex: 1,
+  cacheSize: 256,
 });
 
 export const cartoLight = () => new TileLayer({
   source: new XYZ({
     url: `https://cartodb-basemaps-{a-c}.global.ssl.fastly.net/rastertiles/voyager_nolabels/{z}/{x}/{y}${retina}.png`,
+    crossOrigin: "",
     attributions: [osmAttribution, cartoAttribution],
     tilePixelRatio: DEVICE_PIXEL_RATIO > 1 ? 2 : 1, // Retina support
     maxZoom: 20,
@@ -31,6 +34,7 @@ export const cartoLight = () => new TileLayer({
   base: true,
   preload: Infinity,
   zIndex: 1,
+  cacheSize: 256,
 });
 
 export const osm = () => new TileLayer({
@@ -38,11 +42,13 @@ export const osm = () => new TileLayer({
   base: true,
   preload: Infinity,
   zIndex: 1,
+  cacheSize: 256,
 });
 
 export const cyclosm = () => new TileLayer({
   source: new XYZ({
     url: `https://{a-c}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png`,
+    crossOrigin: "",
     attributions: [osmAttribution, cyclosmAttribution],
     tilePixelRatio: DEVICE_PIXEL_RATIO > 1 ? 2 : 1, // Retina support
     maxZoom: 20,
@@ -50,6 +56,7 @@ export const cyclosm = () => new TileLayer({
   base: true,
   preload: Infinity,
   zIndex: 1,
+  cacheSize: 256,
 });
 
 export const bw = () => new TileLayer({
@@ -58,10 +65,12 @@ export const bw = () => new TileLayer({
     attributions: [osmAttribution, cyclosmAttribution],
     tilePixelRatio: DEVICE_PIXEL_RATIO > 1 ? 2 : 1, // Retina support
     maxZoom: 20,
+    crossOrigin: "",
   }),
   base: true,
   preload: Infinity,
   zIndex: 1,
+  cacheSize: 256,
 });
 
 export function supportsVectorLabels(layer) {
