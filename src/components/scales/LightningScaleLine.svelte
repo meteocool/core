b173e1d5398d11a196a10161f17073d063d3c224
Valentin Dornauer <valentin@unimplemented.org>
2022-05-06 03:40:55 +0200

    Shorten labels in app

--- src/components/scales/LightningScaleLine.svelte
+++ src/components/scales/LightningScaleLine.svelte
@@ -2,6 +2,7 @@
   import ScaleLine from "./ScaleLine.svelte";
   import { radarColormap, unit } from "../../stores";
   import { LightningColors } from "../../colormaps";
+  import { DeviceDetect as dd } from '../../lib/DeviceDetect';
 
   let unique = {};
 
@@ -17,6 +18,7 @@
   });
 
   const legendItems = [1, 2, 3, 5, 20, 30, 60, 90, 120];
+  const isApp = dd.isApp();
 </script>
 
 <style>
@@ -30,6 +32,9 @@
     <ScaleLine class="scale" valueFormat={ (value) => {
       const intValue = parseInt(value, 10);
       if (legendItems.includes(intValue)) {
+        if (isApp) {
+          return `${value} min`;
+        }
         return `${value} <span class="minutes"> Minute${intValue === 1 ? "" : "s"}</span>`;
       }
       return "";
